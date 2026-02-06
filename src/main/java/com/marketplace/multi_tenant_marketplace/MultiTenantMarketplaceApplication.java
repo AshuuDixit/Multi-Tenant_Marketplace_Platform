0249f002b511152f0c19f906068800ad07160a19
AshuuDixit <72594457+AshuuDixit@users.noreply.github.com>
2026-02-06 20:45:43 +0530

    Initial commit - multi tenant marketplace setup

---
+++ src/main/java/com/marketplace/multi_tenant_marketplace/MultiTenantMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.multi_tenant_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketplace/multi_tenant_marketplace/MultiTenantMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.multi_tenant_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
